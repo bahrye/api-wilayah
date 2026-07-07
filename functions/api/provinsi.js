789a3b22b7b95bec90806b286a07b82ac7570c94
Syamsul Bahri <syamsulbahri.agro27b@gmail.com>
2026-07-07 21:26:08 +0800

    chore: ignore sensitive api code and configuration

--- functions/api/provinsi.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest({ env }) {
-  try {
-    // Di format Kemendagri, provinsi memiliki panjang kode 2 digit
-    const { results } = await env.DB.prepare("SELECT * FROM wilayah WHERE length(kode) = 2").all();
-    return new Response(JSON.stringify(results));
-  } catch (e) {
-    return new Response(JSON.stringify({ error: e.message }), { status: 500 });
-  }
-}
